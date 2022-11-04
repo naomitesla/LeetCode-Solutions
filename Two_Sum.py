6c56066c42f6551a5c6efcefa8ce9342eb563c8b
Naomi Tesla <110672478+NaomiTesla@users.noreply.github.com>
2022-11-03 22:41:18 -0400

    fixed a comma issue with two digit outputs!

--- Two_Sum.py
+++ Two_Sum.py
@@ -8,8 +8,8 @@ class Solution(object):
         for i in nums:
             remainder = target - i  
             try:
-                pos_1 = int(nums.index(i))
-                pos_2 = int(nums.index(remainder, pos_1+1))
+                pos_1 = nums.index(i)
+                pos_2 = nums.index(remainder, pos_1+1)
             except:
                 continue
-            return "{0}{1}".format(pos_1, pos_2)
+            return pos_1, pos_2
