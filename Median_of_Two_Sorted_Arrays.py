45d418ae6fb3a19b1ee1c142f07ae0798b11e6b0
Naomi Tesla <110672478+NaomiTesla@users.noreply.github.com>
2022-11-04 00:31:07 -0400

    Solution for Median of Two Sorted Arrays!

---
+++ Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,32 @@
+# Naomi Tesla
+# https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+import math
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        sorted_l = nums1 + nums2
+        sorted_l.sort()
+        length = len(sorted_l)
+
+        if length%2 == 0:
+            if length > 4: 
+                crossed = (length - 2)/2
+            else:
+                crossed = 1
+            if length == 2:
+                first_index = sorted_l[0]
+                last_index = sorted_l[1]
+            else:    
+                first_index = sorted_l[crossed]
+                last_index = sorted_l[crossed+1]
+            result = float(first_index + last_index)/2
+        else:
+            result = sorted_l[int(math.ceil(length/2))]
+    
+        return result
