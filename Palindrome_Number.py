10205655731e7b62920f1fbd5014ad100bab4afc
Naomi Tesla <110672478+NaomiTesla@users.noreply.github.com>
2022-11-26 05:58:35 -0500

    Pretty simple solution for the plaindrome number problem c:

---
+++ Palindrome_Number.py
@@ -0,0 +1,13 @@
+# Naomi Tesla
+# https://leetcode.com/problems/palindrome-number/
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if str(x)[::-1] == str(x):
+            return True
+        else:
+            return False
